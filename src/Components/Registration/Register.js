00ecf1cbcc69ad1e72126b701b0d30353e572c8f
harshad <Harshadkajale333@gmail.com>
2023-01-18 01:01:40 +0530

    Co-authored-by: pavan.p.b.pavan@gmail.com <pavan.p.b.pavan@gmail.com>

--- src/Components/Registration/Register.js
+++ src/Components/Registration/Register.js
@@ -1,7 +1,97 @@
-import React from "react";
-
+import React, { useState } from "react";
+import style from "./Register.module.css";
+import CustomButton from "../../Atom/CustomButton";
+import Input from "../../Atom/Input";
+import { Link } from "react-router-dom";
 function Register() {
-  return <div>Register</div>;
+  const [form, Setform] = useState(false);
+  const [toggle, setToggle] = useState(false);
+  function Form() {
+    Setform(true);
+  }
+  function ToggleEU() {
+    setToggle(!toggle);
+  }
+  return (
+    <div className={style.container}>
+      <div className={style.container1}>
+        <div className={style.mainContainer}>
+          {form ? (
+            <>
+              <div className={style.form}>
+                <div>
+                  <h1>Create your account</h1>
+                </div>
+                <div>
+                  <Input className={style.input1} placeholder="Name" />
+                  <br />
+                  {toggle ? (
+                    <>
+                      <Input className={style.input2} placeholder="Phone" />
+                    </>
+                  ) : (
+                    <>
+                      <Input className={style.input2} placeholder="email" />
+                    </>
+                  )}
+                  <span className={style.toggle} onClick={ToggleEU}>
+                    {toggle ? <> UseEmail insted </> : <>UsePhone insted</>}
+                  </span>
+                </div>
+                <div>
+                  <CustomButton
+                    buttonText="Sign up"
+                    customCss={style.formbtn}
+                  ></CustomButton>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <p>
+                <i class="fa fa-brands fa-twitter"></i>
+              </p>
+              <h1>Join Twitter today</h1>
+              <div>
+                <CustomButton
+                  icon={<i class="fa fa-brands fa-google"></i>}
+                  buttonText="Sign in with google"
+                  customCss={style.CustomButton1}
+                ></CustomButton>
+              </div>
+              <br />
+              <div>
+                <CustomButton
+                  icon={<i class="fa fa-brands fa-apple"></i>}
+                  buttonText="Sign in with Apple"
+                  customCss={style.CustomButton2}
+                />
+              </div>
+              <div className={style.or}>
+                <p className={style.line}>__________</p>
+                OR
+                <p className={style.line}>__________</p>
+              </div>
+              <div className={style.signupbtn}>
+                <CustomButton
+                  btnNext={Form}
+                  buttonText="sign up with phone number or email"
+                  customCss={style.CustomButton2}
+                />
+                <p>
+                  By signing up, you agree to the Terms of Service <br />
+                  and Privacy Policy, including Cookie Use.
+                </p>
+              </div>
+              <div className={style.txt2}>
+                Have an Account Already..? <Link path="/Login"> Log in</Link>
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Register;
